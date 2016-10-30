3223dcb990f4dbf2a4562b735aafee641457e093
RWebRTC <rwebrtc@gmail.com>
2016-10-30 19:08:04 +0800

    create project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RGitHub
+
 RGitHub
+
+# LICENSE
+
+http://renyuzhuo.cn/#/issues/37
+
---
+++ app/src/main/java/cn/renyuzhuo/rgithub/RGitHubMainActivity.java
@@ -0,0 +1,13 @@
+package cn.renyuzhuo.rgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RGitHubMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
